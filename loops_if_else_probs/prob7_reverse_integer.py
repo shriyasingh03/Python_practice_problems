2353c56b35db17d7c9969f5525ce1e1f30d27014
Shriya Singh <2001shriya@gmail.com>
2025-03-21 22:17:12 +0530

    reverse the number

---
+++ loops_if_else_probs/prob7_reverse_integer.py
@@ -0,0 +1,20 @@
+
+# Problem 7 - Reverse a given integer number.
+
+# version1 - 
+n = int(input("enter the number: "))
+
+while n> 0:
+  print(n % 10, end="")
+  n = n //10
+
+
+
+
+
+
+
+
+
+    
+
